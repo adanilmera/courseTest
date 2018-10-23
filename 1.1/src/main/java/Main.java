d43c15ae7ffc831d762d19584634cab03d960210
Artyom Danilin <adanil@mera.ru>
2018-10-23 16:55:42 +0300

    first lesson

---
+++ 1.1/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Frontend allRequestsServlet = new Frontend();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
